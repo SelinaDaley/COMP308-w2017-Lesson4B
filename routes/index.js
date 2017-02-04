af58d78a7d3157c69793ce3d7c51aea5b813c1c6
SelinaDaley <sdmd6@hotmail.com>
2017-02-04 15:05:05 -0500

    a lot of changes

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,43 @@
 let express = require('express');
 let router = express.Router();
 
-// global route variables
+// Global Route Variables
 let currentDate = new Date();
-currentDate = currentDate.toLocaleTimeString();
+  currentDate = currentDate.toLocaleTimeString();
 
 /* GET home page. wildcard */
 router.get('/', (req, res, next) => {
-  res.render('index', { 
-    title: 'Express', 
-    date: currentDate
-  });
+  res.render('content/index', {
+    title: 'Home'
+   });
 });
 
 /* GET about page. */
 router.get('/about', (req, res, next) => {
-  res.render('about', { 
+  res.render('content/about', {
     title: 'About'
-  });
+   });
+});
+
+/* GET products page. */
+router.get('/projects', (req, res, next) => {
+  res.render('content/projects', {
+    title: 'Projects'
+   });
+});
+
+/* GET services page. */
+router.get('/services', (req, res, next) => {
+  res.render('content/services', {
+    title: 'Services'
+   });
+});
+
+/* GET contact page. */
+router.get('/contact', (req, res, next) => {
+  res.render('content/contact', {
+    title: 'Contact'
+   });
 });
 
 module.exports = router;
